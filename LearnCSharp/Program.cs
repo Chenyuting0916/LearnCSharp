1179f1215722cc351c96f3531db1154d7a1c9b73
tim.chen73 <tim.chen@titansoft.com.sg>
2022-08-22 23:07:18 +0800

    init

---
+++ LearnCSharp/Program.cs
@@ -0,0 +1,26 @@
+﻿// reference type v.s. value type
+
+//value type
+
+var a = 10;
+var b = a;
+b++;
+Console.WriteLine("a:{0}, b:{1}", a, b);
+
+
+//reference type
+var array1 = new int[3] { 1, 2, 3 };
+var array2 = array1;
+array2[0] = 0;
+Console.WriteLine("array1[0]:{0}, array2[0]:{1}", array1[0], array2[0]);
+
+
+//try something
+var string1 = "string";
+string string2;
+string2 = string1;
+string2 = "string222222222";
+Console.WriteLine("string1:{0}, string2:{1}", string1, string2);
+
+
+Console.WriteLine("string is primitive? {0}",typeof(string).IsPrimitive);
