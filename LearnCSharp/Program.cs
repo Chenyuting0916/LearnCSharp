8f02a82af9ff827299b081c8518b85a7623baa79
tim.chen73 <tim.chen@titansoft.com.sg>
2022-09-12 18:05:14 +0800

    has a v.s. is a

--- LearnCSharp/Program.cs
+++ LearnCSharp/Program.cs
@@ -1,26 +1,28 @@
-﻿// reference type v.s. value type
-
-//value type
-
-using System.Collections;
-
-var a = 10;
-var b = a;
-b++;
-Console.WriteLine("a:{0}, b:{1}", a, b);
+﻿using System.Collections;
 
 
-//reference type
-var array1 = new int[3] { 1, 2, 3 };
-var array2 = array1;
-array2[0] = 0;
-Console.WriteLine("array1[0]:{0}, array2[0]:{1}", array1[0], array2[0]);
+
+// reference type v.s. value type
+
+    //value type
+    var a = 10;
+    var b = a;
+    b++;
+    Console.WriteLine("a:{0}, b:{1}", a, b);
+
+
+    //reference type
+    var array1 = new int[3] { 1, 2, 3 };
+    var array2 = array1;
+    array2[0] = 0;
+    Console.WriteLine("array1[0]:{0}, array2[0]:{1}", array1[0], array2[0]);
 
 
 //try something
 var string1 = "string";
-string string2;
-string2 = "string222222222";
+    var string2 = string1;
+    string2 = "string222222222";
+    string2 += "333";
 Console.WriteLine("string1:{0}, string2:{1}", string1, string2);
 
 
---
+++ LearnCSharp/Relationship.cs
@@ -0,0 +1,26 @@
+﻿namespace LearnCSharp;
+
+public class Relationship
+{
+    private Dog a = new Dog();
+}
+
+public class Animal
+{
+    
+}
+
+public class Dog : Animal
+{
+    // is a relationship    
+}
+
+
+public class School{
+    // has a relationship
+    public Student Student { get; set; }
+}
+
+public abstract class Student
+{
+}
