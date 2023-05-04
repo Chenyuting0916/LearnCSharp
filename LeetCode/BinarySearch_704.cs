e5323f1a79e82f2b610aa56eacdbec38b00b26ec
Tim Chen <tim376755000@gmail.com>
2023-05-04 08:17:04 +0800

    add 704

---
+++ LeetCode/BinarySearch_704.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCode
+{
+    internal class BinarySearch_704
+    {
+        public int Search(int[] nums, int target)
+        {
+            int left = 0;
+            int right = nums.Length - 1;
+
+            while (right >= left)
+            {
+                int mid = (left + right) / 2;
+                if (target > nums[mid])
+                {
+                    left = mid + 1;
+                }
+                else if (target < nums[mid])
+                {
+                    right = mid - 1;
+                }
+                else
+                {
+                    return mid;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
