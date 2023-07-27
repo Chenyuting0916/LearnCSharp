47f862acedb90a5df75579fa8904d30301aedbfe
Tim Chen <tim376755000@gmail.com>
2023-07-27 23:18:33 +0800

    add 2 leetcode easy problems

---
+++ LeetCode/392IsSubsequence.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    internal class _392IsSubsequence
+    {
+        public bool IsSubsequence(string s, string t)
+        {
+            if (s.Length == 0) return true;
+            int sIndex = 0;
+            for (int i = 0; i < t.Length; i++)
+            {
+                if (t[i] == s[sIndex])
+                {
+                    sIndex++;
+                    if (sIndex == s.Length) return true;
+                }
+            }
+
+            return sIndex == s.Length;
+        }
+    }
+}
---
+++ LeetCode/643MaximumAverageSubarrayI.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    internal class _643MaximumAverageSubarrayI
+    {
+        public double FindMaxAverage(int[] nums, int k)
+        {
+            var max = -Double.MaxValue;
+            var sum = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+
+                sum += nums[i];
+
+                if (i > k - 1)
+                {
+                    sum -= nums[i - k];
+                }
+
+                if (i >= k - 1)
+                {
+                    if (max < (double)sum / k)
+                    {
+                        max = (double)sum / k;
+                    }
+                }
+            }
+
+            return max;
+        }
+    }
+}
