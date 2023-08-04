7111464999a0de55a6092fdb23b760d87919258f
Tim Chen <tim376755000@gmail.com>
2023-08-04 20:56:10 +0800

    add 1 leetcode easy

---
+++ LeetCode/724_Find_Pivot_Index.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    internal class _724_Find_Pivot_Index
+    {
+        public int PivotIndex(int[] nums)
+        {
+            int sum = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                sum += nums[i];
+            }
+
+            int leftSum = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (sum - leftSum - nums[i] == leftSum)
+                {
+                    return i;
+                }
+                leftSum += nums[i];
+            }
+
+            return -1;
+        }
+    }
+}
