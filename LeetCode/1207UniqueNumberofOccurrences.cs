0088c354f1285c71e13cab4bf27086b556cfee31
昱廷 陳 <chenyuting0916@hotmail.com>
2024-01-24 02:28:32 +0800

    add 1207 leetcode

---
+++ LeetCode/1207UniqueNumberofOccurrences.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    internal class _1207UniqueNumberofOccurrences
+    {
+        public bool UniqueOccurrences(int[] arr)
+        {
+            var dic = new Dictionary<int, int>();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (!dic.ContainsKey(arr[i]))
+                {
+                    dic.Add(arr[i], 1);
+                }
+                else
+                {
+                    dic[arr[i]]++;
+                }
+            }
+            var valueList = dic.Values.ToList();
+            return valueList.Count == valueList.Distinct().Count();
+        }
+    }
+}
