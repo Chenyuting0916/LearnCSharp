ce945bf3c9f96fb9a07da9c9686a895823b32132
Tim Chen <tim376755000@gmail.com>
2024-01-27 23:00:40 +0800

    add leetcode 206

---
+++ LeetCode/206ReverseLinkedList.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    internal class _206ReverseLinkedList
+    {
+        public ListNode ReverseList(ListNode head)
+        {
+            ListNode resultNode = null;
+            while (head != null)
+            {
+                resultNode = new ListNode(head.val, resultNode);
+                head = head.next;
+            }
+            return resultNode;
+        }
+
+        public ListNode ReverseList2(ListNode head)
+        {
+            ListNode cur = head;
+            ListNode prev = null;
+            ListNode nxt = null;
+            while (cur != null)
+            {
+                nxt = cur.next;
+                cur.next = prev;
+                prev = cur;
+                cur = nxt;
+            }
+
+            return prev;
+        }
+    }
+}
