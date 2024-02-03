bce416df22d94076d871701b2ff3092615be816f
Tim Chen <tim376755000@gmail.com>
2024-02-03 23:22:41 +0800

    add 104, copied leetocde answer

---
+++ LeetCode/104MaximumDepthofBinaryTree.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    internal class _104MaximumDepthofBinaryTree
+    {
+        public int MaxDepth(TreeNode root)
+        {
+            if (root == null)
+                return 0;
+
+            int left = MaxDepth(root.left);
+            int right = MaxDepth(root.right);
+
+            return Math.Max(left, right) + 1;
+        }
+    }
+}
