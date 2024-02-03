e9bbf4fd10cf5ca862fd6c95bd7ac436bfb0925a
Tim Chen <tim376755000@gmail.com>
2024-02-03 23:24:12 +0800

    add tree node

--- LeetCode/104MaximumDepthofBinaryTree.cs
+++ LeetCode/104MaximumDepthofBinaryTree.cs
@@ -19,4 +19,18 @@ namespace LeetCode
             return Math.Max(left, right) + 1;
         }
     }
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
 }
