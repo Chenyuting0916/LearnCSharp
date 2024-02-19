430c4cbe8094ec55b1c8a8fbf4d7f8f2c033ae2f
Tim Chen <tim376755000@gmail.com>
2024-02-19 19:15:07 +0800

    add 700

---
+++ LeetCode/700SearchinaBinarySearchTree.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    internal class _700SearchinaBinarySearchTree
+    {
+        public TreeNode SearchBST(TreeNode root, int val)
+        {
+            if (root == null) return null;
+
+            if (root.val == val) return root;
+
+            if (root.val < val) return SearchBST(root.right, val);
+
+            else return SearchBST(root.left, val);
+        }
+    }
+}
