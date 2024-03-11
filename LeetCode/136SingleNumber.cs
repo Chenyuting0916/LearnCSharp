695754b19a0dd09e9764fb6c4a9939ba4b8a02db
昱廷 陳 <chenyuting0916@hotmail.com>
2024-03-11 19:04:48 +0800

    add 136

---
+++ LeetCode/136SingleNumber.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    internal class _136SingleNumber
+    {
+        public int SingleNumber(int[] nums)
+        {
+            var list = new List<int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (!list.Contains(nums[i]))
+                {
+                    list.Add(nums[i]);
+                }
+                else
+                {
+                    list.Remove(nums[i]);
+                }
+            }
+
+            return list.First();
+        }
+
+
+        public int SingleNumber2(int[] nums)
+        {
+            var result = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                result ^= nums[i];
+            }
+            return result;
+        }
+    }
+}
