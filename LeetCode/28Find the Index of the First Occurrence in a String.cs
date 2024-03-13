7a3522f0d5a7b80db36060663386cae0f2ad6081
昱廷 陳 <chenyuting0916@hotmail.com>
2024-03-13 21:21:49 +0800

    add 28

---
+++ LeetCode/28Find the Index of the First Occurrence in a String.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    internal class _28Find_the_Index_of_the_First_Occurrence_in_a_String
+    {
+        public int StrStr(string haystack, string needle)
+        {
+            return haystack.IndexOf(needle);
+        }
+    }
+}
