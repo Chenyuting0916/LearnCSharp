60e90a2ed76778278c9b4c052d5dcdc58872e292
Tim Chen <tim376755000@gmail.com>
2024-10-29 00:57:51 +0900

    add 169

---
+++ LeetCode/169MajorityElement.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    internal class _169MajorityElement
+    {
+        public int MajorityElement(int[] nums)
+        {
+            var dictionary = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (!dictionary.ContainsKey(nums[i]))
+                {
+                    dictionary.Add(nums[i], 1);
+                }
+                else
+                {
+                    dictionary[nums[i]] += 1;
+                }
+            }
+            return dictionary.MaxBy(entry => entry.Value).Key; ;
+        }
+    }
+}
