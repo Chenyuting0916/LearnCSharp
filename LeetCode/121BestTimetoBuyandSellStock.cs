2c5506f784c48370c486714f112756d36192d6f6
Tim Chen <tim376755000@gmail.com>
2024-10-31 00:28:36 +0900

    add 121

---
+++ LeetCode/121BestTimetoBuyandSellStock.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    internal class _121BestTimetoBuyandSellStock
+    {
+        public int MaxProfit(int[] prices)
+        {
+            int maxProfit = 0;
+            int lowestPrice = prices[0];
+            for (int i = 1; i < prices.Length; i++)
+            {
+                if (prices[i] < lowestPrice)
+                {
+                    lowestPrice = prices[i];
+                }
+                else if (prices[i] - lowestPrice > maxProfit)
+                {
+                    maxProfit = prices[i] - lowestPrice;
+                }
+            }
+
+            return maxProfit;
+        }
+    }
+}
